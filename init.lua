44935aaaad40e3b659ac742ecdb5ce1b55231734
MATSUU Takuto <matsuu@gmail.com>
2022-04-16 14:11:52 +0900

    initial import

---
+++ init.lua
@@ -0,0 +1,39 @@
+vim.opt.list = true
+-- vim.opt.completeopt = "menu,menuone,noselect,preview"
+vim.g.goimports_simplify = 1
+vim.cmd("colorscheme github")
+
+local cmp = require("cmp")
+
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			vim.fn["vsnip#anonymous"](args.body)
+		end,
+	},
+	window = {
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }),
+	}),
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+		{ name = 'vsnip' },
+		{ name = 'nvim_lsp_signature_help' },
+	}, {
+		{ name = 'buffer' },
+	})
+})
+
+local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+local lsp_installer = require("nvim-lsp-installer")
+
+lsp_installer.on_server_ready(function(server)
+  local opts = { capabilities = capabilities }
+  server:setup(opts)
+end)
