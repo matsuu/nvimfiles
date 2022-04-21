e40f2f5f76d23dffdf5f3573a9a2e56897feb63f
MATSUU Takuto <matsuu@gmail.com>
2022-04-21 20:27:31 +0900

    Remove submodule and use vim-jetpack

--- init.lua
+++ init.lua
@@ -1,38 +1,69 @@
-vim.opt.list = true
-vim.g.goimports_simplify = 1
-vim.cmd("colorscheme github")
+require('jetpack').startup(function(use)
+  use'cormacrelf/vim-colors-github'
+  use'hrsh7th/nvim-cmp'
+  use'hrsh7th/cmp-nvim-lsp'
+  use'hrsh7th/cmp-buffer'
+  use'hrsh7th/cmp-path'
+  use'hrsh7th/cmp-cmdline'
+  use'hrsh7th/cmp-nvim-lsp-signature-help'
+  use'mattn/vim-goimports'
+  use'neovim/nvim-lspconfig'
+  use'williamboman/nvim-lsp-installer'
+end)
 
-local cmp = require("cmp")
+vim.opt.list = true
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+vim.opt.termguicolors = true
+vim.opt.pumblend = 10
+vim.opt.winblend = 10
+-- vim.opt.background = 'light'
+
+vim.cmd('colorscheme github')
+
+vim.g.goimports_simplify = 1
+
+local cmp = require('cmp')
 
 cmp.setup({
-	snippet = {
-		expand = function(args)
-			vim.fn["vsnip#anonymous"](args.body)
-		end,
-	},
-	window = {
-	},
-	mapping = cmp.mapping.preset.insert({
-		['<C-b>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.abort(),
-		['<CR>'] = cmp.mapping.confirm({ select = true }),
-	}),
-	sources = cmp.config.sources({
-		{ name = 'nvim_lsp' },
-		{ name = 'vsnip' },
-		{ name = 'nvim_lsp_signature_help' },
-	}, {
-		{ name = 'buffer' },
-	})
+  snippet = {
+    expand = function(args)
+      vim.fn['vsnip#anonymous'](args.body)
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset,
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' },
+    { name = 'nvim_lsp_signature_help' },
+  }, {
+    { name = 'buffer' },
+  })
 })
 
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
 
-local lsp_installer = require("nvim-lsp-installer")
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+local lsp_installer = require('nvim-lsp-installer')
 
 lsp_installer.on_server_ready(function(server)
-  local opts = { capabilities = capabilities }
-  server:setup(opts)
+  server:setup({ capabilities = capabilities })
 end)
