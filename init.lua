6b7f8d925d75a12c8479ef1a7d8301fdba0a0bcb
MATSUU Takuto <matsuu@gmail.com>
2022-04-22 22:07:05 +0900

    Use packer

--- init.lua
+++ init.lua
@@ -1,4 +1,10 @@
 require('plugins')
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
 
 -- base
 vim.opt.list = true
