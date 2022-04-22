acae0a126f7574612a87e52072c95d78355af51b
MATSUU Takuto <matsuu@gmail.com>
2022-04-22 21:46:52 +0900

    combine plugin configurations

--- init.lua
+++ init.lua
@@ -1,16 +1,4 @@
 require('plugins')
-require('autocomplete')
-require('lualine').setup {
-  options = {
-    theme = 'material',
-    icons_enabled = false,
-    component_separators = '',
-    section_separators = '',
-  },
-}
-vim.cmd('colorscheme base16-bright')
--- vim.opt.background = 'light'
--- vim.cmd('colorscheme base16-google-light')
 
 -- base
 vim.opt.list = true
@@ -18,5 +6,3 @@ vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
 vim.opt.termguicolors = true
 vim.opt.pumblend = 10
 vim.opt.winblend = 10
-
-vim.g.goimports_simplify = 1
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,22 +2,92 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   -- autocomplete
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/cmp-vsnip'
-  use 'hrsh7th/vim-vsnip'
-  use 'hrsh7th/cmp-nvim-lsp-signature-help'
-  use 'neovim/nvim-lspconfig'
-  use 'williamboman/nvim-lsp-installer'
+  use {
+    'hrsh7th/nvim-cmp',
+    requires = {
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/cmp-vsnip',
+      'hrsh7th/vim-vsnip',
+      'hrsh7th/cmp-nvim-lsp-signature-help',
+      'neovim/nvim-lspconfig',
+    },
+    config = function()
+      local cmp = require('cmp')
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            vim.fn['vsnip#anonymous'](args.body)
+          end,
+        },
+        window = {
+          -- completion = cmp.config.window.bordered(),
+          -- documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'vsnip' },
+          { name = 'nvim_lsp_signature_help' },
+        }, {
+          { name = 'buffer' },
+        })
+      })
+      cmp.setup.cmdline('/', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+          { name = 'cmdline' }
+        })
+      })
+    end,
+  }
+  use {
+    'williamboman/nvim-lsp-installer',
+    requires = { 'hrsh7th/cmp-nvim-lsp' },
+    config = function()
+      local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+      local lsp_installer = require('nvim-lsp-installer')
+      lsp_installer.on_server_ready(function(server)
+        server:setup({ capabilities = capabilities })
+      end)
+    end,
+  }
 
   -- colorscheme
-  use 'RRethy/nvim-base16'
+  use {
+    'RRethy/nvim-base16',
+    config = 'vim.cmd[[colorscheme base16-bright]]',
+  }
 
   -- statusline
-  use 'nvim-lualine/lualine.nvim'
+  use {
+    'nvim-lualine/lualine.nvim',
+    config = function()
+      require('lualine').setup {
+        options = {
+          theme = 'material',
+          icons_enabled = false,
+          component_separators = '',
+          section_separators = '',
+        },
+      }
+    end,
+  }
 
   -- filetypes
   use {
@@ -29,5 +99,6 @@ return require('packer').startup(function(use)
     'mattn/vim-goimports',
     opt = true,
     ft = {'go'},
+    setup = 'vim.g.goimports_simplify = 1',
   }
 end)
