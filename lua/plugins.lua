ba6848f6bed7770d59bf9d1a4075459a5450d3a0
MATSUU Takuto <matsuu@gmail.com>
2022-04-26 21:16:31 +0900

    Revert "add crazy8 plugin"
    This reverts commit ddd9ed0234398c7a554b2b6b159246f1db0e9c1e.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -130,9 +130,6 @@ return require('packer').startup(function(use)
 	-- filetypes
 	use { 'fgsch/vim-varnish', opt = true, ft = { 'vcl' } }
 
-	-- tabstop
-	use { 'zsugabubus/crazy8.nvim' }
-
 	-- git
 	use {
 		'sindrets/diffview.nvim',
