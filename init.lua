13a6b94a2fad108db92dc19978241696624dc522
MATSUU Takuto <matsuu@gmail.com>
2022-04-29 00:20:16 +0900

    Add termguicolors for *blend

--- init.lua
+++ init.lua
@@ -3,6 +3,8 @@ require('plugins')
 -- base
 vim.opt.list = true
 vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
+
+vim.opt.termguicolors = true
 vim.opt.pumblend = 10
 vim.opt.winblend = 10
 
