0e1a2606d8c0a85dd131f1687a012e816c97e45e
MATSUU Takuto <matsuu@gmail.com>
2022-05-07 17:04:54 +0900

    Add cursorline and cursorcolumn

--- init.lua
+++ init.lua
@@ -3,6 +3,8 @@ require('plugins')
 -- base
 vim.opt.list = true
 vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = true
 
 if vim.env.TERM_PROGRAM ~= 'Apple_Terminal' then
 	vim.opt.termguicolors = true
