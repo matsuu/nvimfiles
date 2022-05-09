deaf08fb34073dae64ff61c05c01c69f82b4e8e7
MATSUU Takuto <matsuu@gmail.com>
2022-05-09 20:54:32 +0900

    Adjust autocomplete

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -11,9 +11,7 @@ return require('packer').startup(function(use)
 	use {
 		'hrsh7th/nvim-cmp',
 		requires = {
-			'hrsh7th/cmp-buffer',
-			'hrsh7th/cmp-path',
-			'hrsh7th/cmp-cmdline',
+			'hrsh7th/cmp-nvim-lsp',
 			'L3MON4D3/LuaSnip',
 			'saadparwaiz1/cmp_luasnip',
 			'hrsh7th/cmp-nvim-lsp-signature-help',
@@ -42,22 +40,6 @@ return require('packer').startup(function(use)
 					{ name = 'nvim_lsp' },
 					{ name = 'luasnip' },
 					{ name = 'nvim_lsp_signature_help' },
-				}, {
-					{ name = 'buffer' },
-				})
-			})
-			cmp.setup.cmdline('/', {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = {
-					{ name = 'buffer' }
-				}
-			})
-			cmp.setup.cmdline(':', {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = cmp.config.sources({
-					{ name = 'path' }
-				}, {
-					{ name = 'cmdline' }
 				})
 			})
 			vim.diagnostic.config({
@@ -66,8 +48,6 @@ return require('packer').startup(function(use)
 				-- underline = false,
 				-- update_in_insert = true,
 			})
-			vim.opt.updatetime = 250
-			vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
 		end,
 	}
 	use {
