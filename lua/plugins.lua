1d9e2b683314c0f420abeff2488960c8b41f6399
MATSUU Takuto <matsuu@gmail.com>
2022-05-11 20:27:38 +0900

    Support placceholders/callSnippet

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -19,10 +19,11 @@ return require('packer').startup(function(use)
 		},
 		config = function()
 			local cmp = require('cmp')
+			local luasnip = require('luasnip')
 			cmp.setup({
 				snippet = {
 					expand = function(args)
-						require('luasnip').lsp_expand(args.body)
+						luasnip.lsp_expand(args.body)
 					end,
 				},
 				window = {
@@ -34,7 +35,28 @@ return require('packer').startup(function(use)
 					['<C-f>'] = cmp.mapping.scroll_docs(4),
 					['<C-Space>'] = cmp.mapping.complete(),
 					['<C-e>'] = cmp.mapping.abort(),
-					['<CR>'] = cmp.mapping.confirm({ select = true }),
+					['<CR>'] = cmp.mapping.confirm({
+						behavior = cmp.ConfirmBehavior.Replace,
+						select = true,
+					}),
+					['<Tab>'] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, { 'i', 's' }),
+					['<S-Tab>'] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { 'i', 's' }),
 				}),
 				sources = cmp.config.sources({
 					{ name = 'nvim_lsp' },
@@ -69,7 +91,13 @@ return require('packer').startup(function(use)
 						diagnostics = {
 							globals = { 'vim' },
 						},
-					}
+						completion = {
+							callSnippet = 'Replace',
+						},
+					},
+					gopls = {
+						usePlaceholders = true,
+					},
 				}
 			}
 
